87f5a1022dcfb1da21fa3f558bf1815357ff535c
Leamur5 <leamur5@mail.ru>
2023-05-06 16:19:16 +0300

    added dates for parsing, deleted segment

--- postgres/CreateDB.sql
+++ postgres/CreateDB.sql
@@ -3,8 +3,8 @@ CREATE TABLE IF NOT EXISTS public."Users"
     chat_id bigint NOT NULL,
     datetime timestamp without time zone NOT NULL,
     platform character varying(50),
-    segment character varying(50),
+    start_date date,
+    end_date date,
     brand character varying(100),
-    subscribtion boolean,
     request_category character varying(100)
 )
